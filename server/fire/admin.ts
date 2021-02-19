02a91c9f65833496a3b33dc02673d77da90ccc82
daniel-sjkdm <daniel.py.216@gmail.com>
2021-02-18 22:55:51 -0600

    Use variables from config files

--- server/fire/admin.ts
+++ server/fire/admin.ts
@@ -1,32 +1,14 @@
 import admin from "firebase-admin";
-//import serviceAccount from "../config/firebase.json"
-const serviceAccount = require("../config/firebase.json");
-//import serviceAccountJson from ""
-require("dotenv").config();
+import config from "config";
 
-const FIREBASE_PRIVATE_KEY = process.env.FIREBASE_PRIVATE_KEY.replace(
-	/\\n/g,
-	"\n"
-)
-	.replace("-----BEGIN PUBLIC KEY-----", "")
-	.replace("-----END PUBLIC KEY-----", "");
-const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID;
-const FIREBASE_CLIENT_EMAIL = process.env.FIREBASE_CLIENT_EMAIL;
+const serviceAccount: admin.ServiceAccount = config.get(
+	"FIREBASE.SERVICE_ACCOUNT"
+);
+const databaseURL: string = config.get("FIREBASE.CLIENT.databaseURL");
 
-//console.log("Service accounttt")
-//console.log(serviceAccount)
-/** 
-admin.initializeApp({
-    credential: admin.credential.cert({
-        privateKey: FIREBASE_PRIVATE_KEY,
-        projectId: FIREBASE_PROJECT_ID,
-        clientEmail: FIREBASE_CLIENT_EMAIL
-    })
-});
-*/
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
-	databaseURL: "https://deployment-test-302915-default-rtdb.firebaseio.com/",
+	databaseURL: databaseURL,
 });
 
 const db = admin.database();
